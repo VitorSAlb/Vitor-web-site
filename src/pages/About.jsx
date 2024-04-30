a5c75b89efb65e580d2e43f679285928fae0b7ce
VitorSAlb <vitor.s.albq@gmail.com>
2024-04-30 16:02:42 -0300

    Update photo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header/Header";
 import Tittle from "../components/Tittles/Tittles";
 import Perfil from "../components/Perfil/Perfil";
 import SmallCard from "../components/SmallCard/SmallCard"
-import fotoPerfil from '../assets/fotoCurriculo.jpeg';
+import fotoPerfil from '../assets/ftCurriculo.jpg';
 import '../styles/global.scss'
 
 const texto = "Software engineer with 2 years of experience in web and mobile application development. I started in the development field at 18 years old when I entered university and deepened my knowledge to levels I never imagined! Based on feedback from people close to me, I consider myself extremely proactive, dedicated, hardworking, responsible and passionate about learning everything about the IT field. I'm currently studying Science Computer in UNIFACS."
